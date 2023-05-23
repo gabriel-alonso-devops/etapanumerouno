e182b963c90274ae057bbaba507fad71ad8702aa
leomencho <leobollini@gmail.com>
2023-05-23 11:01:11 -0300

    primerContador super simple suma y resta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'comercios';
+  public title: string = 'comercios';
+  numero: number=10;
 }
