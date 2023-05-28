371e1af9530eccf132fd1623bf3627de1bd9d3c3
leomencho <leobollini@gmail.com>
2023-05-28 01:24:16 -0300

    practica10 con base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'comercios';
+  numero : number =10;
+  base : number=5
+
+  acumular ( valor: number){
+    this.numero += valor;
+  }
 }
